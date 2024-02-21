767601eb8cef120c0e4c8005d7dd6e30f5fa2eb1
Ankit Tripathi <95411215+ANKITTRIPATHI99@users.noreply.github.com>
2024-02-21 06:13:49 +0000

    variables let, var and const

---
+++ jsbasics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let  accountEmail = "tripathiankkit991@gmail.com"
+var accountPassword = "hello123"
+accountCity = "delhi"
+let accountState;
+
+ accountEmail = "tripathi@gmail.com"
+  accountPassword = "Hi123"
+  accountcity = "noida"
+
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountState])
