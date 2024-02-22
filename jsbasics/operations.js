836a496968f3a8a6ba2253bec929bf9ac7fc8bce
Ankit Tripathi <95411215+ANKITTRIPATHI99@users.noreply.github.com>
2024-02-22 06:33:34 +0000

    operations in js

---
+++ jsbasics/operations.js
@@ -0,0 +1,27 @@
+// let value = 3
+// let negvalue = -value
+
+
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(5%2)
+
+// let str1 = "Ankit"
+// let str2 = " Tripathi"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+// console.log((1 + 2 )+ "3")
+
+// try to solve in brackets
+
+
