bbd5be788a42eefaca77bfa711909a9d6183093d
Ankit Tripathi <95411215+ANKITTRIPATHI99@users.noreply.github.com>
2024-02-27 20:07:42 +0000

    Numbers

---
+++ jsbasics/Numbersandmaths.js
@@ -0,0 +1,28 @@
+const score = 400;
+console.log(score);
+
+const balance = new  Number(400);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const othernumber = 123.4355;
+console.log(othernumber.toPrecision(5));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString()); //foreign methods of count
+console.log(hundreds.toLocaleString('en-IN')); //indian system of counting
+
+
+
+//***********************************Maths*******************************************8*//
+
+
+
+
+
+
+
