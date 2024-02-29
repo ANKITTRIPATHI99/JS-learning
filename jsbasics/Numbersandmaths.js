3af94ef57a6c7fce39e41480a8f18e57058e9d62
Ankit Tripathi <95411215+ANKITTRIPATHI99@users.noreply.github.com>
2024-02-29 14:04:51 +0000

    maths in js

--- jsbasics/Numbersandmaths.js
+++ jsbasics/Numbersandmaths.js
@@ -1,24 +1,51 @@
-const score = 400;
-console.log(score);
+// const score = 400;
+// console.log(score);
 
-const balance = new  Number(400);
-console.log(balance);
+// const balance = new  Number(400);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toString().length);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
 
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
 
-const othernumber = 123.4355;
-console.log(othernumber.toPrecision(5));
+// const othernumber = 123.4355;
+// console.log(othernumber.toPrecision(5));
 
-const hundreds = 10000000;
-console.log(hundreds.toLocaleString()); //foreign methods of count
-console.log(hundreds.toLocaleString('en-IN')); //indian system of counting
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString()); //foreign methods of count
+// console.log(hundreds.toLocaleString('en-IN')); //indian system of counting
 
 
 
 //***********************************Maths*******************************************8*//
+
+// console.log(Math);
+// console.log(Math.abs(-6)); // it gives a absolute value
+// console.log(Math.round(7.66));
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(8.9));
+// console.log(Math.min(7, 8 ,6));
+// console.log(Math.max(7, 8 ,6));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) +1);
+// console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max -min +1) +min));
+
+
+
+
+
+
+
+
+
+
 
 
 
