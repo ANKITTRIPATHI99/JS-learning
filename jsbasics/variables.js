bd1a63a6a6bb2519009dee7d4aa8ef59467dfba0
Ankit Tripathi <tripathiankit7991@gmail.com>
2025-11-12 20:41:18 +0530

    Set accountState and update email/password
    Initialize accountState with a value and update accountEmail and accountPassword.

--- jsbasics/variables.js
+++ jsbasics/variables.js
@@ -2,7 +2,7 @@ const accountId = 1234
 let  accountEmail = "tripathiankkit991@gmail.com"
 var accountPassword = "hello123"
 accountCity = "delhi"
-let accountState;
+let accountState=10;
 
  accountEmail = "tripathi@gmail.com"
   accountPassword = "Hi123"
